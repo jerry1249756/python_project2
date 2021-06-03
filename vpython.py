7e5585a4745b7a889471f47ce77de1c82a6a0f2a
HelloJohnny822 <80762886+HelloJohnny822@users.noreply.github.com>
2021-06-03 16:20:58 +0800

    Add files via upload

---
+++ vpython.py
@@ -0,0 +1,28 @@
+from vpython import *
+screen = [[0 for i in range(150)]for j in range(40)]
+
+def light(chr, x ) :
+    if chr == '0':
+        screen (0 , 0) = 0
+        screen (0 , 1) = 1
+        screen (0 , 2) = 1
+        screen (0 , 3) = 1
+        screen (0 , 4) = 0
+        screen (1 , 0) = 
+        screen (1 , 1) = 
+        screen (1 , 2) = 
+        screen (1 , 3) = 
+        screen (1 , 4) = 
+        screen (2 , 0) = 
+        screen (2 , 1) = 
+
+    if 
+
+
+
+
+
+line1=input("please input line 1 ")
+length_of_line1 = len(line1)
+for i in range (length_of_line1) :
+    light(len[i] , i)
