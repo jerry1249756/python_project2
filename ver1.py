266450df60eb2aa72bafc5e01959504a5ff43f09
jerry1249756 <jerry1249756@gmail.com>
2021-06-03 16:02:04 +0800

    fix E_dir

--- ver1.py
+++ ver1.py
@@ -1,12 +1,12 @@
 from vpython import*
 import numpy
 
-Ex=0.003
-Ey=0.006
+Ey=0.003
+Ez=0.006
 d=5 #distance from board to screen
 L=4 #size of the board
 h=3 #distance between the board
-q=1.6E-19
+q=1.6E-19 
 m=9.11E-31
 v_init=0.05
 rt=1000  #rate
@@ -20,17 +20,20 @@ downboard = box(width=L,length=L, height=0.01, pos=vec(0,-h/2,0))
 screen = box(width=L, length=0.01, height=L, pos=vec(h/2+d,0,0), color=color.white)
 projector = box(width=0.4,length=3,height=0.4, pos=vec(-d,0,0))
 
-E_field = arrow(pos=vec(0,0,0), axis=500*vec(Ex,Ey,0), color=color.green)
+E_field = arrow(pos=vec(0,0,0), axis=500*vec(0,Ey,Ez), color=color.green)
 ball = sphere(radius=0.2, pos=vec(-d+1.5,0,0),v=vec(v_init,0,0))
+
+
 
 t=0
 dt=1/rt
 while True:
     t+=dt
     if(ball.pos.x<h/2+d):
-        ball.pos.y+=ball.v.y*dt
         ball.pos.x+=ball.v.x*dt
+        ball.pos.y+=ball.v.y*dt
+        ball.pos.z+=ball.v.z*dt
     if(ball.pos.x>-L/2 and ball.pos.x<L/2):
         ball.v.y+=Ey/10*dt
-        ball.v.x+=Ex/10*dt
+        ball.v.z+=Ez/10*dt
 
